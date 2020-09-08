5a8d4a880b69292d5199474b1118155b02ba1fa2
alfianjau <alfianjauhari@gmail.com>
2020-09-08 12:11:58 +0700

    chore: code split reducer action

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,30 +1,4 @@
 import axios from 'axios'
-// create redux like mutation
-export function fetchReducer(state, action) {
-  switch (action.type) {
-    case 'FETCH_START':
-      return {
-        ...state,
-        isLoading: true,
-        hasError: false,
-      }
-    case 'FETCH_SUCCESS':
-      return {
-        ...state,
-        isLoading: false,
-        hasError: false,
-        articles: action.payload,
-      }
-    case 'FETCH_FAILURE':
-      return {
-        ...state,
-        isLoading: false,
-        hasError: true,
-      }
-    default:
-      throw new Error()
-  }
-}
 
 // create async function for axios instance
 export async function fetchHits(query, dispatch, cancelToken) {
